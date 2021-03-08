904b53e32990b8c57141fff32d3960f8a07d5c35
ZEIDA DE LOS REYES RODRIGUEZ MENDOZA <zeida.rodriguez@ikeasi.com>
2021-03-08 13:56:18 +0000

    Initial commit

---
+++ src/main/java/com/serverbitboxer2/serverbitboxer2/Serverbitboxer2Application.java
@@ -0,0 +1,13 @@
+package com.serverbitboxer2.serverbitboxer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Serverbitboxer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Serverbitboxer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/serverbitboxer2/serverbitboxer2/Serverbitboxer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverbitboxer2.serverbitboxer2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Serverbitboxer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
